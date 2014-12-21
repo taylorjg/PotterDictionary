13f2c42ed95837cf47fa417b32a491a08f9496de
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-12-21 16:40:57 +0000

    A few little refactorings

---
+++ Code/HarryPotterBooks.cs
@@ -0,0 +1,14 @@
+﻿namespace Code
+{
+    public static class HarryPotterBooks
+    {
+        public static readonly string[] Titles = new[]
+            {
+                "Harry Potter and the Philosopher's Stone",
+                "Harry Potter and the Chamber of Secrets",
+                "Harry Potter and the Prisoner of Azkaban",
+                "Harry Potter and the Goblet of Fire",
+                "Harry Potter and the Order of the Phoenix"
+            };
+    }
+}
--- PropertyTests/PotterDictionaryPropertyTests.cs
+++ PropertyTests/PotterDictionaryPropertyTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Code;
 using FsCheck;
 using FsCheck.Fluent;
+using NUnit.Framework;
 
 namespace PropertyTests
 {
@@ -10,70 +11,72 @@ namespace PropertyTests
     {
         private const decimal UnitPrice = PotterCalculator.UnitPrice;
 
-        private static readonly string[] HarryPotterBookTitles = new[]
-            {
-                "Harry Potter and the Philosopher's Stone",
-                "Harry Potter and the Chamber of Secrets",
-                "Harry Potter and the Prisoner of Azkaban",
-                "Harry Potter and the Goblet of Fire",
-                "Harry Potter and the Order of the Phoenix"
-            };
+        private static bool TitlesAreDifferent(ICollection<string> titles)
+        {
+            return titles.Distinct().Count() == titles.Count;
+        }
 
-        private static readonly IEnumerable<string[]> AllCombinationsOfTwoDifferentBooks =
-            from title1 in HarryPotterBookTitles
-            from title2 in HarryPotterBookTitles
+        private static readonly IEnumerable<string[]> CombinationsOfTwoDifferentBooks =
+            from title1 in HarryPotterBooks.Titles
+            from title2 in HarryPotterBooks.Titles
             let titles = new[] {title1, title2}
-            where titles.Distinct().Count() == titles.Length
+            where TitlesAreDifferent(titles)
             select titles;
 
-        private static readonly IEnumerable<string[]> AllCombinationsOfThreeDifferentBooks =
-            from title1 in HarryPotterBookTitles
-            from title2 in HarryPotterBookTitles
-            from title3 in HarryPotterBookTitles
+        private static readonly IEnumerable<string[]> CombinationsOfThreeDifferentBooks =
+            from title1 in HarryPotterBooks.Titles
+            from title2 in HarryPotterBooks.Titles
+            from title3 in HarryPotterBooks.Titles
             let titles = new[] {title1, title2, title3}
-            where titles.Distinct().Count() == titles.Length
+            where TitlesAreDifferent(titles)
             select titles;
 
-        private static readonly IEnumerable<string[]> AllCombinationsOfFourDifferentBooks =
-            from title1 in HarryPotterBookTitles
-            from title2 in HarryPotterBookTitles
-            from title3 in HarryPotterBookTitles
-            from title4 in HarryPotterBookTitles
+        private static readonly IEnumerable<string[]> CombinationsOfFourDifferentBooks =
+            from title1 in HarryPotterBooks.Titles
+            from title2 in HarryPotterBooks.Titles
+            from title3 in HarryPotterBooks.Titles
+            from title4 in HarryPotterBooks.Titles
             let titles = new[] {title1, title2, title3, title4 }
-            where titles.Distinct().Count() == titles.Length
+            where TitlesAreDifferent(titles)
             select titles;
 
-        private static readonly IEnumerable<string[]> AllCombinationsOfFiveDifferentBooks =
-            from title1 in HarryPotterBookTitles
-            from title2 in HarryPotterBookTitles
-            from title3 in HarryPotterBookTitles
-            from title4 in HarryPotterBookTitles
-            from title5 in HarryPotterBookTitles
+        private static readonly IEnumerable<string[]> CombinationsOfFiveDifferentBooks =
+            from title1 in HarryPotterBooks.Titles
+            from title2 in HarryPotterBooks.Titles
+            from title3 in HarryPotterBooks.Titles
+            from title4 in HarryPotterBooks.Titles
+            from title5 in HarryPotterBooks.Titles
             let titles = new[] {title1, title2, title3, title4, title5 }
-            where titles.Distinct().Count() == titles.Length
+            where TitlesAreDifferent(titles)
             select titles;
 
-        private static readonly Gen<string> GenOneTitle = Gen.elements(HarryPotterBookTitles);
+        private static readonly Gen<string> GenOneTitle = Gen.elements(HarryPotterBooks.Titles);
 
         private static readonly Gen<string[]> GenMultipleBooksTheSame =
             GenOneTitle.SelectMany(
                 title => Gen.choose(1, 10).Select(
                     n => Enumerable.Repeat(title, n).ToArray()));
 
-        private static readonly Gen<string[]> GenTwoDifferentTitles = Gen.elements(AllCombinationsOfTwoDifferentBooks);
-        private static readonly Gen<string[]> GenThreeDifferentTitles = Gen.elements(AllCombinationsOfThreeDifferentBooks);
-        private static readonly Gen<string[]> GenFourDifferentTitles = Gen.elements(AllCombinationsOfFourDifferentBooks);
-        private static readonly Gen<string[]> GenFiveDifferentTitles = Gen.elements(AllCombinationsOfFiveDifferentBooks);
+        private static readonly Gen<string[]> GenTwoDifferentTitles = Gen.elements(CombinationsOfTwoDifferentBooks);
+        private static readonly Gen<string[]> GenThreeDifferentTitles = Gen.elements(CombinationsOfThreeDifferentBooks);
+        private static readonly Gen<string[]> GenFourDifferentTitles = Gen.elements(CombinationsOfFourDifferentBooks);
+        private static readonly Gen<string[]> GenFiveDifferentTitles = Gen.elements(CombinationsOfFiveDifferentBooks);
 
         private static bool CheckPrice(IEnumerable<string> titles, decimal expectedPrice)
         {
-            var actualPrice = PotterCalculator.CalculatePrice(titles.Select(title => new Book(title)).ToArray());
+            var books = titles.Select(title => new Book(title)).ToArray();
+            var actualPrice = PotterCalculator.CalculatePrice(books);
             return actualPrice == expectedPrice;
         }
 
         private static bool CheckPrice(string title, decimal expectedPrice)
         {
             return CheckPrice(new[] {title}, expectedPrice);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
         }
 
         [FsCheck.NUnit.Property]
